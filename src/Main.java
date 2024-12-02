6254f6fd6d7ac8d44b81eda494db19af59e68370
jezi89 <13301753+jezi89@users.noreply.github.com>
2024-12-02 18:58:54 +0100

    Eerste deel opdracht gemaakt

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,22 @@
+// Import the HashMap class
+import java.util.HashMap;
+
 
 public class Main {
     public static void main(String[] args) {
+//        To use other types, such as int, you must specify an equivalent wrapper class (dus hoofdletters): Integer. For other primitive types, use: Boolean for boolean, Character for char, Double for double, etc:
+        HashMap<Integer, String> convertedNumber = new HashMap <Integer,String> ();
+        convertedNumber.put(1, "één");
+        convertedNumber.put(2, "twee");
+        convertedNumber.put(3, "drie");
+        convertedNumber.put(4, "vier");
+        convertedNumber.put(5, "vijf");
+        convertedNumber.put(6, "zes");
+        convertedNumber.put(7, "zeven");
+        convertedNumber.put(8, "acht");
+        convertedNumber.put(9, "negen");
 
-        // Hier mag je je code schrijven voor de hoofd-opdracht
-
-
+        int roundedValue =  InputHandler.readInput();;
+        System.out.println(convertedNumber.get(roundedValue));
     }
 }
